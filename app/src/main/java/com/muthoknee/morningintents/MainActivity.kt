33e08a2e64d2ee45d9c23d06389226a5d17d6625
mutho-knee <mwangimichelle911@gmail.com>
2023-04-05 07:49:31 +0300

    first commit

---
+++ app/src/main/java/com/muthoknee/morningintents/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.muthoknee.morningintents
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mSms: Button
+    lateinit var mEmail: Button
+    lateinit var mCamera: Button
+    lateinit var mShare: Button
+    lateinit var mMpesa: Button
+    lateinit var mCall: Button
+    lateinit var mWebsite: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mSms=findViewById(R.id.mBtnsms)
+        mEmail=findViewById(R.id.mBtnEmail)
+        mCamera=findViewById(R.id.mBtnCamera)
+        mShare=findViewById(R.id.mBtnShare)
+        mMpesa=findViewById(R.id.mBtnMpesa)
+        mCall=findViewById(R.id.mBtnCall)
+        mWebsite=findViewById(R.id.mBtnWebsite)
+        // set on click
+        mSms.setOnClickListener{
+            val uri: Uri = Uri.parse("smsto:0712345678")
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+            intent.putExtra("sms_body", "The SMS text")
+            startActivity(intent)
+        }
+        mEmail.setOnClickListener{
+            val emailIntent =
+                Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "mish@gmail.com", null))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Body")
+            startActivity(Intent.createChooser(emailIntent, "Send email..."))
+        }
+        mCamera.setOnClickListener{
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePictureIntent, 1)
+        }
+        mShare.setOnClickListener{
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Hey, download this app from https://www.mish.com!")
+            startActivity(shareIntent)
+        }
+        mMpesa.setOnClickListener{
+            val simToolKitLaunchIntent =
+                applicationContext.packageManager.getLaunchIntentForPackage("com.android.stk")
+            simToolKitLaunchIntent?.let { startActivity(it) }
+        }
+        mCall.setOnClickListener{
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "0712345678"))
+            if (ContextCompat.checkSelfPermission(
+                    this@MainActivity,
+                    android.Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf<String>(android.Manifest.permission.CALL_PHONE),
+                    1
+                )
+            } else {
+                startActivity(intent)
+            }
+        }
+        mWebsite.setOnClickListener{
+        val tembea = Intent(this@MainActivity, WebsiteActivity::class.java)
+            startActivity(tembea)
+        }
+    }
+}
---
+++ app/src/main/java/com/muthoknee/morningintents/WebsiteActivity.kt
@@ -0,0 +1,11 @@
+package com.muthoknee.morningintents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WebsiteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_website)
+    }
+}
